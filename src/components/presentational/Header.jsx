704485b03720b1e0f22c9586aebd6873b92a99a3
Mark Edomwande <medomwande@gmail.com>
2019-02-25 17:53:54 +0100

    style complete
    - cards styled
    - links redirect to external page
    - limit to fifteen books

--- src/components/presentational/Header.jsx
+++ src/components/presentational/Header.jsx
@@ -3,8 +3,8 @@ import React, { Fragment } from 'react';
 const Header = () => {
   return (
     <Fragment>
-      <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>
+      <div className="app-title">
+        <h4 className="my-0 mr-md-auto font-weight-normal text-center text-uppercase">Book Finder</h4>
       </div>
     </Fragment>
   );
